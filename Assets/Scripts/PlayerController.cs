495ffea2171f08c20650b466ede422adb65c03fe
devonnc <dechavez@s.sfusd.edu>
2023-02-21 16:05:41 -0800

    Revert "finished unit 3 starting unit 3 challenge"
    This reverts commit 5235b62499f1083308f11601f625b0c34a8c89d1.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,11 +6,6 @@ public class PlayerController : MonoBehaviour
 {
     private Rigidbody playerRb;
     private Animator playerAnim;
-    private AudioSource playerAudio;
-    public ParticleSystem explosionParticle;
-    public ParticleSystem dirtParticle;
-    public AudioClip jumpSound;
-    public AudioClip crashSound;
     public float jumpForce = 10;
     public float gravityModifier;
     public bool isOnGround = true;
@@ -20,7 +15,6 @@ public class PlayerController : MonoBehaviour
     {
         playerRb = GetComponent<Rigidbody>();
         playerAnim = GetComponent<Animator>();
-        playerAudio = GetComponent<AudioSource>();
         Physics.gravity *= gravityModifier;
 
        
@@ -34,8 +28,6 @@ public class PlayerController : MonoBehaviour
              playerRb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
              isOnGround = false;
              playerAnim.SetTrigger("Jump_trig");
-             dirtParticle.Stop();
-             playerAudio.PlayOneShot(jumpSound, 1.0f);
 
         }
     }
@@ -47,17 +39,13 @@ public class PlayerController : MonoBehaviour
         if (collision.gameObject.CompareTag("Ground"))
         {
             isOnGround = true;
-            dirtParticle.Play();
         } else if (collision.gameObject.CompareTag("Obstacle"))
         {
             Debug.Log("Game Over");
             gameOver = true;
             playerAnim.SetBool("Death_b", true);
             playerAnim.SetInteger("DeathType_int", 1);
-            explosionParticle.Play();
-            dirtParticle.Stop();
-            playerAudio.PlayOneShot(crashSound, 1.0f);
-     }
+        }
     }
 }
 
